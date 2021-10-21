04e5f19ad07c749fff432b84f475c0c5bc27a8fb
Vadim Dobrin <zuka537122@gmail.com>
2021-10-21 18:10:05 +0300

    Final part

--- src/com/company/vehicles/Lorry.java
+++ src/com/company/vehicles/Lorry.java
@@ -1,4 +1,22 @@
 package com.company.vehicles;
 
+public class Lorry extends Car {
+    private int carrying;
 
+    public int getCarrying() {
+        return carrying;
+    }
+
+    public void setCarrying(int carrying) {
+        this.carrying = carrying;
+    }
+
+    @Override
+    public String toString() {
+        return "Lorry{" +
+                "engine=" + engine +
+                ", driver=" + driver +
+                ", carrying=" + carrying +
+                '}';
+    }
 }
--- src/com/company/vehicles/SportCar.java
+++ src/com/company/vehicles/SportCar.java
@@ -1,4 +1,22 @@
 package com.company.vehicles;
 
+public class SportCar extends Car {
+    private double speed;
 
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "SportCar{" +
+                "engine=" + engine +
+                ", driver=" + driver +
+                ", speed=" + speed +
+                '}';
+    }
 }
