d89b3e25a2003bcf7c33a39292a036b472276d92
Vadim Dobrin <zuka537122@gmail.com>
2021-10-21 16:35:25 +0300

    First part

---
+++ src/com/company/professions/Driver.java
@@ -0,0 +1,21 @@
+package com.company.professions;
+
+public class Driver extends Person {
+
+    private int experience ;
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "experience=" + experience +
+                '}';
+    }
+}
---
+++ src/com/company/vehicles/Car.java
@@ -0,0 +1,5 @@
+package com.company.vehicles;
+
+public class Car {
+
+}
