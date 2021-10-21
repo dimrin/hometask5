9c05080544d13630f4e65a5b007f4c41d06575c7
Vadim Dobrin <zuka537122@gmail.com>
2021-10-21 18:09:45 +0300

    Final part

--- src/com/company/professions/Person.java
+++ src/com/company/professions/Person.java
@@ -1,30 +1,4 @@
 package com.company.professions;
 
-public class Person {
-    private int age;
-    private String fullName;
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "age=" + age +
-                ", fullName='" + fullName + '\'' +
-                '}';
-    }
 }
--- src/com/company/vehicles/Lorry.java
+++ src/com/company/vehicles/Lorry.java
@@ -1,22 +1,4 @@
 package com.company.vehicles;
 
-public class Lorry extends Car {
-    private int carrying;
 
-    public int getCarrying() {
-        return carrying;
-    }
-
-    public void setCarrying(int carrying) {
-        this.carrying = carrying;
-    }
-
-    @Override
-    public String toString() {
-        return "Lorry{" +
-                "engine=" + engine +
-                ", driver=" + driver +
-                ", carrying=" + carrying +
-                '}';
-    }
 }
